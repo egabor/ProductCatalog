08ba4ae54e4e30356cd9d27f324517ad96a19bb0
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-06-29 20:21:47 +0200

    Resolver setup

---
+++ beverages/Resolver+Registry.swift
@@ -0,0 +1,14 @@
+//
+//  Resolver+Registry.swift
+//  beverages
+//
+//  Created by Eszenyi Gábor on 2023. 06. 29..
+//
+
+import Foundation
+import Resolver
+
+extension Resolver: ResolverRegistering {
+
+    public static func registerAllServices() {}
+}
