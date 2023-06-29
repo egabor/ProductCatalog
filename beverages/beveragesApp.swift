df097a9c0df3b473d847e185bb9a498026c066a3
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-06-29 20:12:32 +0200

    Initial Commit

---
+++ beverages/beveragesApp.swift
@@ -0,0 +1,17 @@
+//
+//  beveragesApp.swift
+//  beverages
+//
+//  Created by Eszenyi Gábor on 2023. 06. 29..
+//
+
+import SwiftUI
+
+@main
+struct beveragesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
