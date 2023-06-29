62f7e42854d9928485d004e0df5b78afae063b0c
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-06-29 21:00:40 +0200

    Basic navigation added

--- beverages/Products/ProductList/ProductListScreen.swift
+++ beverages/Products/ProductList/ProductListScreen.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct ProductListScreen: View {
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationTitle("Products") // TODO: Localize
+            .toolbar(content: addNewProductButton)
+    }
+
+    func addNewProductButton() -> some View {
+        NavigationLink {
+            ProductDetailsScreen()
+        } label: {
+            Image(systemName: "plus") // TODO: Move to constant
+        }
     }
 }
 
